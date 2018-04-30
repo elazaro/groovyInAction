cdf48b9e8cfe2dd394c9d33aaa51369bab2cb621
elazaro <elazaro@mxrisc.com>
2018-04-29 20:42:31 -0500

    First exercises commit.

---
+++ Part 1/Ch2/ApacheCommons.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+
+@Grab('commons-lang:commons-lang:2.4')
+import org.apache.commons.lang.ClassUtils
+
+class Outer {
+   class Inner {}
+}
+
+assert !ClassUtils.isInnerClass(Outer)
+assert ClassUtils.isInnerClass(Outer.Inner)
+
+System.out.println("ssup!");
