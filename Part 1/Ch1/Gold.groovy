74e363169703e03378260bbf55c70845777377c4
elazaro <elazaro@mxrisc.com>
2021-07-28 00:00:15 -0500

    Personal Access Token test.

--- Part 1/Ch1/Gold.groovy
+++ Part 1/Ch1/Gold.groovy
@@ -8,6 +8,7 @@ while(!isGolden(gold[-1])) {
   gold.add( fibo[-1] / fibo[-2] )
 }
 
+
 println "found golden ratio with fibo(${fibo.size-1}) as"
 println fibo[-1] + " / " + fibo[-2] + "=" + gold[-1]
 println "_" * 10 + "|" + "_" * (10 * gold[-1]
